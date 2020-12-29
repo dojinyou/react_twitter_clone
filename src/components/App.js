e33a9690ffc511f2dad98ad23d4f364eed84dc38
DOJIN <djyou128@gmail.com>
2020-12-29 23:18:08 +0900

    1.2 sign in with email, google, github  and sign out

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState, useEffect } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fBase';
+
 function App() {
-	return <AppRouter />;
+	const [init, setInit] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	useEffect(() => {
+		authService.onAuthStateChanged((user) => {
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true);
+		});
+	}, []);
+	return (
+		<>
+			{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initalizing...'}
+			<footer>&copy; {new Date().getFullYear()} Nwiiter </footer>
+			{/* <div>
+				apiKey: {process.env.REACT_APP_API_KEY}
+				<br />
+				authDomain: {process.env.REACT_APP_AUTH_DOMAIN}
+				<br />
+				projectId: {process.env.REACT_APP_PROJECT_ID}
+				<br />
+				storageBucket: {process.env.REACT_APP_STORAGE_BUCKET}
+				<br />
+				messagingSenderId: {process.env.REACT_APP_MESSAGING_ID}
+				<br />
+				appId: {process.env.REACT_APP_APP_ID}
+			</div> */}
+		</>
+	);
 }
 
 export default App;
---
+++ src/fBase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// const firebaseConfig = {
+// 	apiKey: process.env.REACT_APP_API_KEY,
+// 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// 	projectId: process.env.REACT_APP_PROJECT_ID,
+// 	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+// 	appId: process.env.REACT_APP_APP_ID,
+// };
+const firebaseConfig = {
+	apiKey: 'AIzaSyAdbRpxY2JBzM8N3eT0VPo1kJDgeAytBmY',
+	authDomain: 'nwitter-56554.firebaseapp.com',
+	projectId: 'nwitter-56554',
+	storageBucket: 'nwitter-56554.appspot.com',
+	messagingSenderId: '161018011334',
+	appId: '1:161018011334:web:5ff05928cde17c087f01c7',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+
+export const authService = firebase.auth();
