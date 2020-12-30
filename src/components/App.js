188fa1c060510f7b7c53dcfa38b5b1735d8526bf
DOJIN <djyou128@gmail.com>
2020-12-30 23:09:41 +0900

    update nweet delete and edit

--- src/components/App.js
+++ src/components/App.js
@@ -5,33 +5,23 @@ import { authService } from 'fBase';
 function App() {
 	const [init, setInit] = useState(false);
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [userObj, setUserObj] = useState(null);
 	useEffect(() => {
 		authService.onAuthStateChanged((user) => {
 			if (user) {
-				setIsLoggedIn(true);
-			} else {
-				setIsLoggedIn(false);
+				setUserObj(user);
 			}
 			setInit(true);
 		});
 	}, []);
 	return (
 		<>
-			{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initalizing...'}
+			{init ? (
+				<AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+			) : (
+				'Initalizing...'
+			)}
 			<footer>&copy; {new Date().getFullYear()} Nwiiter </footer>
-			{/* <div>
-				apiKey: {process.env.REACT_APP_API_KEY}
-				<br />
-				authDomain: {process.env.REACT_APP_AUTH_DOMAIN}
-				<br />
-				projectId: {process.env.REACT_APP_PROJECT_ID}
-				<br />
-				storageBucket: {process.env.REACT_APP_STORAGE_BUCKET}
-				<br />
-				messagingSenderId: {process.env.REACT_APP_MESSAGING_ID}
-				<br />
-				appId: {process.env.REACT_APP_APP_ID}
-			</div> */}
 		</>
 	);
 }
